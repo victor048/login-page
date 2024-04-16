80d590ace0ab3373afaa37f06c603a097adc3175
victor048 <victor.lacerda.m3@gmaail.com>
2024-04-16 20:44:17 -0300

    Login-page front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./pages/login/login.component";
+import { DefaultLoginLayoutComponent } from "./components/default-login-layout/default-login-layout.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, LoginComponent]
 })
 export class AppComponent {
   title = 'login-page';
